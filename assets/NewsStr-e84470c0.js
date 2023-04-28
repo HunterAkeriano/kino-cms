a0ede7b85d19b0f770dd607643110c8350b3a864
HunterAkeriano <stajectgamer@gmail.com>
2023-04-28 09:16:11 +0300

    исправление багов созвона

--- assets/NewsStr-e84470c0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,d as o}from"./index-6729bfec.js";const n={mounted(){document.title="Admin | Новости - Страница"}};function c(r,s,a,_,d,p){return t(),o("h2",null,"Страница новости")}const m=e(n,[["render",c]]);export{m as default};
--- assets/Statistic-efe34b95.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as e,d as o}from"./index-6729bfec.js";const n={mounted(){document.title="Admin | Статистика"}};function c(s,r,a,_,d,i){return e(),o("h2",null,"Статистика")}const l=t(n,[["render",c]]);export{l as default};
