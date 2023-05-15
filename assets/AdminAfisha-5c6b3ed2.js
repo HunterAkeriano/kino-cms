fc3571f7b1fe5d3ac6dc5b7bd5324e124dd4c007
HunterAkeriano <stajectgamer@gmail.com>
2023-05-15 09:51:55 +0300

    переделал создание фильмов

---
+++ assets/AdminAfisha-5c6b3ed2.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,d as n}from"./index-1d59545f.js";const o={};function r(t,s){return c(),n("h2",null,"Афиша")}const _=e(o,[["render",r]]);export{_ as default};
--- assets/Users-d67981fa.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,d as o}from"./index-ef99c11b.js";const n={mounted(){document.title="Admin | Страницы - Пользователи"}};function s(c,r,a,_,d,p){return t(),o("h2",null,"Пользователи")}const m=e(n,[["render",s]]);export{m as default};
