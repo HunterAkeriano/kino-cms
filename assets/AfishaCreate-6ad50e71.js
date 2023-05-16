611f9f05e5eea65748d23940cf5af51449c15377
HunterAkeriano <stajectgamer@gmail.com>
2023-05-16 17:28:30 +0300

    Афиша главной

---
+++ assets/AfishaCreate-6ad50e71.js
@@ -0,0 +1,1 @@
+import{K as e,f as s,d as t,G as a,o as r,g as l}from"./index-9c2c99fe.js";const _={__name:"AfishaCreate",setup(n){return e(),s(),t(),a(null),a(null),(o,c)=>(r(),l("h2",null,"Создание Афиши"))}};export{_ as default};
